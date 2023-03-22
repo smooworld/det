c37b4078fd4b883ab656dc250532deb35b67a652
Samuel <smooworld2001@yahoo.com>
2023-03-23 01:09:02 +0200

    first commit

---
+++ details.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+print("This is " + name + " he is " + age + " years old and lives at house number " + house_number + " on " + street_name)
